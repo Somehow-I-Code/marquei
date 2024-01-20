d0ed47c5004b1226f0862b6462054e0e4a224f1a
Rafael <rafael2606arantes@gmail.com>
2024-01-20 16:11:07 -0300

    Adicionei o enter para separar o import dos componentes e retirei a div.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 "use client"
 import "./styles.css"
+
 export default function Home() {
   return (
-    <div>
       <button onClick={function(){
         alert("Olá Mundo")
       }}>Primeiro botão</button>
-    </div>
   );
 }

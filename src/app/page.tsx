cd7cfabf0b3618656c2c5a86071f6f398f57ce68
Jaqueline <jaqfreitas.amaral@gmail.com>
2024-01-31 16:42:13 -0300

    Rolagem de Categorias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,43 @@ import "./style.css";
 export default function Home() {
   return (
     <div>
-      <header>
+      <header className="header">
         <Image src={imageLogoMarquei} alt="The logo Marquei" />
-        <h2 className="show">Nome</h2>
       </header>
+      <span className="show">Nome</span>
+
+      <ul className="listCategoria">
+        <li>
+          <button className="cat"> Categoria 1 </button>
+        </li>
+        <li>
+          <button className="cat"> Categoria 2 </button>
+        </li>
+        <li>
+          <button className="cat"> Categoria 3 </button>
+        </li>
+        <li>
+          <button className="cat"> Categoria 4 </button>
+        </li>
+        <li>
+          <button className="cat"> Categoria 5 </button>
+        </li>
+        <li>
+          <button className="cat"> Categoria 6 </button>
+        </li>
+        <li>
+          <button className="cat"> Categoria 7 </button>
+        </li>
+        <li>
+          <button className="cat"> Categoria 8 </button>
+        </li>
+        <li>
+          <button className="cat"> Categoria 9 </button>
+        </li>
+        <li>
+          <button className="cat"> Categoria 10 </button>
+        </li>
+      </ul>
     </div>
   );
 }

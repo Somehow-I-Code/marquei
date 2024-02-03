5b3c4e712193efb276c404a15479bed0e535c054
Jaqueline <jaqfreitas.amaral@gmail.com>
2024-02-03 15:43:57 -0300

    update branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,44 +5,48 @@ import "./style.css";
 
 export default function Home() {
   return (
-    <div>
-      <header className="header">
-        <Image src={imageLogoMarquei} alt="The logo Marquei" />
-      </header>
-      <span className="show">Nome</span>
+    <main>
+      <div>
+        <header className="page-header">
+          <div className="header-image-container">
+            <Image src={imageLogoMarquei} alt="The logo Marquei" />
+          </div>
+          <span className="header-user-tag">Nome</span>
+        </header>
 
-      <ul className="listCategoria">
-        <li>
-          <button className="cat"> Categoria 1 </button>
-        </li>
-        <li>
-          <button className="cat"> Categoria 2 </button>
-        </li>
-        <li>
-          <button className="cat"> Categoria 3 </button>
-        </li>
-        <li>
-          <button className="cat"> Categoria 4 </button>
-        </li>
-        <li>
-          <button className="cat"> Categoria 5 </button>
-        </li>
-        <li>
-          <button className="cat"> Categoria 6 </button>
-        </li>
-        <li>
-          <button className="cat"> Categoria 7 </button>
-        </li>
-        <li>
-          <button className="cat"> Categoria 8 </button>
-        </li>
-        <li>
-          <button className="cat"> Categoria 9 </button>
-        </li>
-        <li>
-          <button className="cat"> Categoria 10 </button>
-        </li>
-      </ul>
-    </div>
+        <ul className="listCategories">
+          <li>
+            <button className="cat"> Categoria 1 </button>
+          </li>
+          <li>
+            <button className="cat"> Categoria 2 </button>
+          </li>
+          <li>
+            <button className="cat"> Categoria 3 </button>
+          </li>
+          <li>
+            <button className="cat"> Categoria 4 </button>
+          </li>
+          <li>
+            <button className="cat"> Categoria 5 </button>
+          </li>
+          <li>
+            <button className="cat"> Categoria 6 </button>
+          </li>
+          <li>
+            <button className="cat"> Categoria 7 </button>
+          </li>
+          <li>
+            <button className="cat"> Categoria 8 </button>
+          </li>
+          <li>
+            <button className="cat"> Categoria 9 </button>
+          </li>
+          <li>
+            <button className="cat"> Categoria 10 </button>
+          </li>
+        </ul>
+      </div>
+    </main>
   );
 }

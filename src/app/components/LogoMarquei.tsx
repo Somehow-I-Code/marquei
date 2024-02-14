0992a3036d2ccbb4d69ddc9e880d0f719a663f8c
Jaqueline <jaqfreitas.amaral@gmail.com>
2024-02-14 10:51:05 -0300

    Add component logo

---
+++ src/app/components/LogoMarquei.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import imageLogoMarquei from "../assets/LOGO_MARQUEI.png";
+
+export function LogoMarquei() {
+  return <Image src={imageLogoMarquei} alt="The logo Marquei" />;
+}

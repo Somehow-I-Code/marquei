bbc4dec938ab0d2af555b343e86fa2889933c60b
Rafael <rafael2606arantes@gmail.com>
2024-02-18 20:59:29 -0300

    Add button component

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
+  variant: "primary" | "transparent";
+}
+
+const styles = {
+  primary: "px-4 py-2 rounded bg-purple-950",
+  transparent: "px-4 py-2 rounded bg-transparent",
+};
+
+export default function Button({variant,className,children,...props}: ButtonProps) {
+  return (
+    <button className={`${styles[variant]} ${className}`} {...props}>
+      {children}
+    </button>
+  );
+}

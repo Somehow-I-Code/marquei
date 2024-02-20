c01d05bac13e1402071d99677f6175da8ade8be4
Jaqueline <jaqfreitas.amaral@gmail.com>
2024-02-20 17:43:44 -0300

    Add pill de categories

--- src/app/components/CategoriesList.tsx
+++ src/app/components/CategoriesList.tsx
@@ -1,15 +1,19 @@
+import Pill from "./Pill";
+
 type CategoriesListProps = {
   list: Array<string>;
 };
 
 export default function CategoriesList({ list }: CategoriesListProps) {
+  const active = "Categoria 1";
+
   return (
     <nav>
-      <ul className="flex gap-5 px-6 overflow-auto hide-scrollbar">
+      <ul className="flex items-center gap-5 px-6 overflow-auto hide-scrollbar">
         {list?.map((category) => {
           return (
             <li key={category} className="whitespace-nowrap">
-              {category}
+              {active === category ? <Pill>{category}</Pill> : category}
             </li>
           );
         })}

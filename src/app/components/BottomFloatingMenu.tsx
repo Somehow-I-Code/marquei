601837cd1c2f9ced92a912f778c6e10f7e5a4649
gisele <gisele.gfa89@gmail.com>
2024-02-20 19:52:46 -0300

    SCRUM-17-add-lucide-icons

--- src/app/components/BottomFloatingMenu.tsx
+++ src/app/components/BottomFloatingMenu.tsx
@@ -1,24 +1,20 @@
-import {
-  FilledPlusIcon,
-  MenuHamburgerIcon,
-  SearchIcon,
-} from "@/components/Icons";
+import { AlignJustify, Plus, SearchIcon } from "lucide-react";
 import Button from "./Button";
 
 export default function BottomFloatingMenu() {
   return (
     <section className="bg-indigo-950 fixed bottom-2 left-4 right-4 flex justify-between py-2 px-5 items-center rounded-full">
       <Button variant="transparent">
-        <SearchIcon />
+        <SearchIcon className="text-slate-50" />
       </Button>
 
       <Button variant="white" className="flex items-center gap-1 rounded-full">
-        <FilledPlusIcon />
+        <Plus className="text-indigo-950" />
         <p className="text-indigo-950 font-bold">Agendar</p>
       </Button>
 
       <Button variant="transparent">
-        <MenuHamburgerIcon />
+        <AlignJustify className="text-slate-50" />
       </Button>
     </section>
   );

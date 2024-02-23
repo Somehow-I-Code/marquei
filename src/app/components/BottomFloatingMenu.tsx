12b0337e753e6bec79db7d09f9be1465ec18acb0
Rafael <rafael2606arantes@gmail.com>
2024-02-23 01:34:04 -0300

    SCRUM-19-add-shadcn-button-component

--- src/app/components/BottomFloatingMenu.tsx
+++ src/app/components/BottomFloatingMenu.tsx
@@ -1,19 +1,20 @@
 import { Menu, Plus, SearchIcon } from "lucide-react";
-import Button from "./Button";
+import { Button } from "@/components/ui/button";
+
 
 export default function BottomFloatingMenu() {
   return (
     <section className="bg-indigo-950 fixed bottom-2 left-4 right-4 flex justify-between py-2 px-5 items-center rounded-full">
-      <Button variant="transparent">
+      <Button variant="ghost">
         <SearchIcon className="text-slate-50" />
       </Button>
 
-      <Button variant="white" className="flex items-center gap-1 rounded-full">
+      <Button variant="outline" className="flex items-center gap-1 rounded-full">
         <Plus />
         <p className="text-indigo-950 font-bold text-xs">Agendar</p>
       </Button>
 
-      <Button variant="transparent">
+      <Button variant="ghost">
         <Menu className="text-slate-50" />
       </Button>
     </section>

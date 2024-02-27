170ae4b42a6feede934e7cb1badc5373289aae66
gisele <gisele.gfa89@gmail.com>
2024-02-27 16:30:56 -0300

    create resources endpoint
    in this commit creates resources endpoint that will be used in the future to retrieve all relevant data from each user's resources

--- api/index.ts
+++ api/index.ts
@@ -6,6 +6,66 @@ server.get("/hello", async (request, reply) => {
   reply.send({ hello: "thiago" });
 });
 
+server.get("/resources", async (request, reply) => {
+  reply.send({
+    data: [
+      {
+        name: "Resource #1",
+        description: "#1 Here we have some text helping describe that resource",
+        category: "Médicos",
+      },
+
+      {
+        name: "Resource #2",
+        description: "#2 Here we have some text helping describe that resource",
+        category: "Médicos",
+      },
+
+      {
+        name: "Resource #3",
+        description: "#3 Here we have some text helping describe that resource",
+        category: "Médicos",
+      },
+
+      {
+        name: "Resource #4",
+        description: "#4 Here we have some text helping describe that resource",
+        category: "Médicos",
+      },
+
+      {
+        name: "Resource #5",
+        description: "#5 Here we have some text helping describe that resource",
+        category: "Médicos",
+      },
+
+      {
+        name: "Resource #6",
+        description: "#6 Here we have some text helping describe that resource",
+        category: "Exames",
+      },
+
+      {
+        name: "Resource #7",
+        description: "#7 Here we have some text helping describe that resource",
+        category: "Exames",
+      },
+
+      {
+        name: "Resource #8",
+        description: "#8 Here we have some text helping describe that resource",
+        category: "Exames",
+      },
+
+      {
+        name: "Resource #9",
+        description: "#9 Here we have some text helping describe that resource",
+        category: "Resultados",
+      },
+    ],
+  });
+});
+
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
     console.error(err);

ecc2fefca8671cb210e4a73ff300dc82ccc1de72
gisele <gisele.gfa89@gmail.com>
2024-03-10 18:30:13 -0300

    hover:bg-accent removed in button component

--- web/src/app/components/bottom-floating-menu.tsx
+++ web/src/app/components/bottom-floating-menu.tsx
@@ -4,19 +4,19 @@ import { Menu, Plus, SearchIcon } from "lucide-react";
 export default function BottomFloatingMenu() {
   return (
     <section className="bg-indigo-950 fixed bottom-2 left-4 right-4 flex justify-between py-2 px-5 items-center rounded-full">
-      <Button variant="ghost" className="hover:bg-indigo-950">
+      <Button variant="ghost">
         <SearchIcon className="text-slate-50" />
       </Button>
 
       <Button
         variant="outline"
-        className="flex items-center gap-1 rounded-full hover:bg-slate-50"
+        className="flex items-center gap-1 rounded-full"
       >
         <Plus />
         <p className="text-indigo-950 font-bold text-xs">Agendar</p>
       </Button>
 
-      <Button variant="ghost" className="hover:bg-indigo-950">
+      <Button variant="ghost">
         <Menu className="text-slate-50" />
       </Button>
     </section>

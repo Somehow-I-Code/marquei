9c81f9ee7f1025fb5e16fd3b5cdab97a4f145280
Rafael <rafael2606arantes@gmail.com>
2024-03-13 12:05:30 -0300

    Add Schedule Drawer Component

--- web/src/app/components/bottom-floating-menu.tsx
+++ web/src/app/components/bottom-floating-menu.tsx
@@ -1,15 +1,6 @@
 import { Button } from "@/components/ui/button";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
-import { Menu, Plus, SearchIcon } from "@/components/ui/icons";
+import { Menu, SearchIcon } from "@/components/ui/icons";
+import ScheduleDrawer from "./schedule-drawer";
 
 export default function BottomFloatingMenu() {
   return (
@@ -18,37 +9,7 @@ export default function BottomFloatingMenu() {
         <SearchIcon className="text-slate-50" />
       </Button>
 
-      <Drawer>
-        <DrawerTrigger>
-          <Button
-            variant="outline"
-            className="flex items-center gap-1 rounded-full"
-          >
-            <Plus />
-            <p className="text-indigo-950 font-bold text-xs">Agendar</p>
-          </Button>
-        </DrawerTrigger>
-
-        <DrawerContent>
-          <DrawerHeader>
-            <DrawerTitle>
-              Esse é o placeholder do botão de agendamento.
-            </DrawerTitle>
-            <DrawerDescription>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
-              recusandae ipsum facilis placeat explicabo illum quia quibusdam
-              fuga! Possimus quam sunt saepe exercitationem voluptatum molestias
-              praesentium in aliquid, minima laboriosam!
-            </DrawerDescription>
-          </DrawerHeader>
-          <DrawerFooter>
-            <Button className="bg-indigo-950">Agendar</Button>
-            <DrawerClose>
-              <Button variant="outline">Cancelar</Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
+      <ScheduleDrawer />
 
       <Button variant="ghost">
         <Menu className="text-slate-50" />
@@ -56,3 +17,4 @@ export default function BottomFloatingMenu() {
     </section>
   );
 }
+

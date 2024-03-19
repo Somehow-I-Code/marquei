7c7d77767d8ce2c0bfbec93ffbae7e157cded4f9
Jaqueline <jaqfreitas.amaral@gmail.com>
2024-03-19 15:49:26 -0300

    Arrumando a formatação

--- api/src/http/routes/create-category.ts
+++ api/src/http/routes/create-category.ts
@@ -3,21 +3,12 @@ import { z } from "zod";
 import categoriesRepository from "../../repositories/categories";
 
 export async function createCategory(server: FastifyInstance) {
-  //Rota para criar uma nova categoria
   server.post("/categories", async (request, reply) => {
     const createCategoryBody = z.object({
       title: z.string(),
     });
 
-    //Fazer uma validação
     const { title } = createCategoryBody.parse(request.body);
-
-    // const category = await prisma.category.create({
-    //   //O dado que vai ser inserido na tabela
-    //   data: {
-    //     title,
-    //   },
-    // });
 
     const category = await categoriesRepository.create({
       title,
--- api/src/repositories/categories.ts
+++ api/src/repositories/categories.ts
@@ -4,24 +4,23 @@ import { CreateCategoryInput } from "../validators/categories";
 class CategoriesRepository {
     async create({ title }: CreateCategoryInput) {
         const category = await prisma.category.create({
-        //O dado que vai ser inserido na tabela
-        data: {
-            title,
-        },
-    });
+            data: {
+                title,
+            },
+        });
 
-    return category;
+        return category;
     }
 
     async findAll() {
-    const categories = await prisma.category.findMany({
-      include: {
-        Resource: true,
-      },
-    });
+        const categories = await prisma.category.findMany({
+            include: {
+                Resource: true,
+            },
+        });
 
-    return categories;
-  }
+        return categories;
+    }
 }
 
 const categoriesRepository = new CategoriesRepository();

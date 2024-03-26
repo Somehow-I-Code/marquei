e8a2d143ea012fb1747e3ec3987d8d717c717f62
Rafael <rafael2606arantes@gmail.com>
2024-03-26 14:00:37 -0300

    Change file name and component name

---
+++ web/src/app/new-resource/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function NewResource() {
+    return (
+        <section className="flex flex-col justify-center items-center h-screen">
+            <div className="flex flex-col items-center space-y-2">
+                <h1 className="text-xl font-bold">
+                    Tela para adicionar recursos
+                </h1>
+                <Button>
+                    <Link href="/">Voltar para home</Link>
+                </Button>
+            </div>
+        </section>
+    );
+}

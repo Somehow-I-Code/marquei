90ce8bbc3a25fd09a686be941c6d338e650a1fcd
gisele <gisele.gfa89@gmail.com>
2024-04-26 21:27:05 -0300

    Change a few veriable names for clarity

--- web/src/app/new-resource/page.tsx
+++ web/src/app/new-resource/page.tsx
@@ -23,16 +23,16 @@ export default async function NewResource() {
             categoryId: Number(data.category),
         };
 
-        const resource = await fetch("http://api:8080/resources", {
+        const response = await fetch("http://api:8080/resources", {
             method: "post",
             headers: {
                 "content-type": "application/json",
             },
             body: JSON.stringify(body),
         });
+        console.log(response);
+        const resource = await response.json();
         console.log(resource);
-        const result = await resource.json();
-        console.log(result);
     }
 
     return (

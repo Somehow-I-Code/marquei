286ab6867f33067d49957d1f44acc53a2c21f79d
Rafael <rafael2606arantes@gmail.com>
2024-04-30 11:28:53 -0300

    Validate fields from request.body with zod

--- api/src/http/routes/login.ts
+++ api/src/http/routes/login.ts
@@ -1,23 +1,39 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { prisma } from "../../lib/prisma";
+
+const loginSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8),
+});
 
 export async function login(server: FastifyInstance) {
     server.post("/login", async (request, reply) => {
-        const user = await prisma.user.findUnique({
-            where: {
-                email: "rafael2606arantes@gmail.com",
-                password: "12345678",
-            },
-            select: {
-                email: true,
-                password: true,
-            },
-        });
+        try {
+            const { email, password } = loginSchema.parse(request.body);
 
-        if (user) {
-            reply.code(200).send({ message: "Usuário encontrado" });
-        } else {
-            reply.code(401).send({ message: "Usuário não encontrado" });
+            const user = await prisma.user.findUnique({
+                where: {
+                    email: email,
+                    password: password,
+                },
+                select: {
+                    email: true,
+                    password: true,
+                },
+            });
+
+            if (user) {
+                return reply.code(200).send({ message: "Usuário encontrado" });
+            } else {
+                return reply
+                    .code(401)
+                    .send({ message: "Usuário não encontrado" });
+            }
+        } catch (error: any) {
+            return reply
+                .code(400)
+                .send({ error: "Erro de validação: " + error.message });
         }
     });
 }

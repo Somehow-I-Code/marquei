979ea93c64a6a84d6eb5bd2291b6ce987d027496
Rafael <rafael2606arantes@gmail.com>
2024-04-30 13:45:28 -0300

    Edit login.ts file and create login-form.ts validator

--- api/src/http/routes/login.ts
+++ api/src/http/routes/login.ts
@@ -1,39 +1,34 @@
 import { FastifyInstance } from "fastify";
-import { z } from "zod";
 import { prisma } from "../../lib/prisma";
-
-const loginSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(8),
-});
+import { loginSchema } from "../../validators/login-form";
 
 export async function login(server: FastifyInstance) {
     server.post("/login", async (request, reply) => {
+
+        let credentials;
+
         try {
-            const { email, password } = loginSchema.parse(request.body);
-
-            const user = await prisma.user.findUnique({
-                where: {
-                    email: email,
-                    password: password,
-                },
-                select: {
-                    email: true,
-                    password: true,
-                },
-            });
-
-            if (user) {
-                return reply.code(200).send({ message: "Usuário encontrado" });
-            } else {
+            credentials = loginSchema.parse(request.body);
+        } catch (error) {
+            if (error instanceof Error) {
                 return reply
-                    .code(401)
-                    .send({ message: "Usuário não encontrado" });
+                    .code(400)
+                    .send({ error: "Erro de validação: " + error.message });
             }
-        } catch (error: any) {
-            return reply
-                .code(400)
-                .send({ error: "Erro de validação: " + error.message });
         }
+
+        const { email, password } = credentials;
+        const user = await prisma.user.findUnique({
+            where: {
+                email: email,
+            },
+        });
+
+        if (!user) {
+            return reply.code(401).send({ message: "Credenciais inválidas" });
+        }
+
+        return reply.code(200).send({ message: "Usuário encontrado" });
     });
 }
+

f63b90c30e19205f9150a7fc3ff742260f08cedd
Jaqueline <jaqfreitas.amaral@gmail.com>
2024-05-07 15:09:26 -0300

    Comp profile e definido cor button ui do sair

--- web/src/app/preview-profile/components/email-profile.tsx
+++
@@ -1,8 +0,0 @@
-export default function EmailPreviewProfile() {
-    return (
-        <div className="flex justify-between items-center mb-2">
-            <span className="text-gray-400 text-sm">E-mail</span>
-            <span className="text-base">usuario@exemplo.com</span>
-        </div>
-    );
-}
--- web/src/app/preview-profile/components/profile-level.tsx
+++
@@ -1,8 +0,0 @@
-export default function ProfileLevelPreview() {
-    return (
-        <div className="flex justify-between items-center">
-            <span className="text-gray-400 text-sm">Nível de perfil</span>
-            <span className="text-base">user</span>
-        </div>
-    );
-}

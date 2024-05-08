097d6654a5de19e9f84283ca031bdd25b3064e8d
Rafael <rafael2606arantes@gmail.com>
2024-05-08 11:14:09 -0300

    Create the jwt with email and profile level data

--- api/src/http/routes/login.ts
+++ api/src/http/routes/login.ts
@@ -1,10 +1,17 @@
 import bcrypt from "bcrypt";
 import { FastifyInstance } from "fastify";
+import jwt from "jsonwebtoken";
 import { prisma } from "../../lib/prisma";
 import { LoginInput, loginSchema } from "../../validators/login-form";
 
 export async function login(server: FastifyInstance) {
     server.post("/login", async (request, reply) => {
+        if (typeof process.env.JWT_SECRET !== "string") {
+            return reply
+                .code(500)
+                .send({ message: "Configuração de token não aplicada" });
+        }
+
         let credentials: LoginInput;
 
         try {
@@ -33,9 +40,11 @@ export async function login(server: FastifyInstance) {
             return reply.code(401).send({ message: "Credenciais inválidas" });
         }
 
-        return reply
-            .code(200)
-            .send({ message: "Credenciais válidadas com sucesso!" });
+        const token = jwt.sign(
+            { email: user.email, profileLevel: user.profileLevel },
+            process.env.JWT_SECRET,
+        );
+
+        return reply.code(200).send({ token });
     });
 }
-

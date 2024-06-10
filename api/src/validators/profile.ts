af018ec557e165a5ad845debc1aa5fb7a75a36cc
Rafael <rafael2606arantes@gmail.com>
2024-06-10 12:06:11 -0300

    Add new type of user SUDO

--- api/src/validators/profile.ts
+++ api/src/validators/profile.ts
@@ -4,7 +4,7 @@ export const createProfileSchema = z.object({
     name: z.string(),
     occupation: z.string(),
     email: z.string(),
-    level: z.union([z.literal("USER"), z.literal("ADMIN")]),
+    level: z.union([z.literal("USER"), z.literal("ADMIN"), z.literal("SUDO")]),
 });
 
 export type CreateProfileInput = z.infer<typeof createProfileSchema>;

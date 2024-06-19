f09409205d7d92d2c444c12e0cf783ea781d1dd6
Rafael <rafael2606arantes@gmail.com>
2024-06-19 00:02:49 -0300

    Create reset password token

--- api/src/http/routes/reset-password.ts
+++ api/src/http/routes/reset-password.ts
@@ -1,24 +1,41 @@
 import { FastifyInstance } from "fastify";
+import jwt from "jsonwebtoken";
 import profileRepository from "../../repositories/profiles";
 import {
     ResetPassword,
     resetPasswordSchema,
 } from "../../validators/reset-password";
 
+const _1_HOUR = 1000 * 60 * 60;
+
 export async function resetPassword(server: FastifyInstance) {
     server.post("/reset-password", async (request, reply) => {
+        if (process.env.JWT_SECRET === undefined) {
+            return reply
+                .status(500)
+                .send({ message: "JWT SECRET não configurado" });
+        }
+
         let resetPasswordEmail: ResetPassword;
 
         resetPasswordEmail = resetPasswordSchema.parse(request.body);
 
         const { email } = resetPasswordEmail;
 
-        const user = await profileRepository.findByEmail(email);
+        const profile = await profileRepository.findByEmail(email);
 
-        if (!user) {
-            return reply.status(404).send({ error: "Email não encontrado" });
+        if (!profile) {
+            return reply.status(404).send({ message: "Email não encontrado" });
         }
+
+        const token = jwt.sign(
+            {
+                id: profile.id,
+                email: profile.email,
+                expiresAt: Date.now() + _1_HOUR,
+            },
+            process.env.JWT_SECRET,
+        );
+        return reply.code(200).send({ token });
     });
 }
-
-

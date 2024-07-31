9953360fa259b972cff1553775e0f8b3de4e3a99
jaqAmaral <jaqfreitas.amaral@gmail.com>
2024-07-31 16:12:49 -0300

    Tela de sucesso

---
+++ web/src/app/success-password-1st-login/page.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import SuccessImage from "./assets/success.svg";
+
+export default async function SuccessPasswordFirstLogin() {
+    return (
+        <section className="mt-48">
+            <div className="flex items-center justify-center ">
+                <SuccessImage />
+            </div>
+
+            <div>
+                <p className="flex flex col gap-y-12 p-12 items-center justify-center font-sans font-extrabold text-3xl text-indigo-950">
+                    Tudo certinho!
+                </p>
+            </div>
+
+            <div>
+                <p className="flex flex col gap-6 items-center justify-center text-base font-bold text-indigo-950">
+                    Pronto para começar a agendar?
+                </p>
+
+                <div className="flex flex-col p-6 gap-2">
+                    <Button className="font-bold text-base">
+                        <Link href="">VAMOS LÁ!</Link>
+                    </Button>
+                    <Button
+                        asChild
+                        className="font-bold text-base bg-white text-indigo-950 border border-indigo-950"
+                    >
+                        <Link href="/">MAIS TARDE</Link>
+                    </Button>
+                </div>
+            </div>
+        </section>
+    );
+}

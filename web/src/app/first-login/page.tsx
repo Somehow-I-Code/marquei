f602d22d40309fa52af0499c8e7ab23dd099764a
Rafael <rafael2606arantes@gmail.com>
2024-08-05 10:38:53 -0300

    Making suggested changes

--- web/src/app/first-login/page.tsx
+++ web/src/app/first-login/page.tsx
@@ -1,16 +1,17 @@
 import CompanyLogo from "../components/company-logo";
+import WelcomeImage from "./assets/welcome.svg";
 import FirstLoginForm from "./components/first-login-reset-password";
 
 export default async function FirstLogin() {
     return (
         <section className="py-12 px-6 space-y-12">
             <div className="flex flex-col items-center">
-                <p className="font-semibold">Bem vindo ao</p>
+                <p className="font-semibold">Bem-vindo ao</p>
                 <CompanyLogo />
             </div>
 
             <div className="items-center">
-                <img src="/images/welcome.svg" alt="Imagem de boas vindas" />
+                <WelcomeImage />
             </div>
 
             <div className="space-y-8">

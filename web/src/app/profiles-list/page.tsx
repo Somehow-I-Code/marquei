fe55142c06b4c3c49938f663cc7cd4f44c3cbdf9
gisele <gisele.gfa89@gmail.com>
2024-08-25 21:44:28 -0300

    Alterando o css do profile-list, usndo divs e flex para deixar de acordo com o layout do figma

--- web/src/app/profiles-list/page.tsx
+++ web/src/app/profiles-list/page.tsx
@@ -5,21 +5,19 @@ import {
     AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
     Card,
     CardContent,
-    CardDescription,
     CardFooter,
     CardHeader,
-    CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Search } from "lucide-react";
+import { Label } from "@/components/ui/label";
+import { Plus, Search } from "lucide-react";
 import Link from "next/link";
 import CompanyLogo from "../components/company-logo";
-import { Label } from "@/components/ui/label";
-import { Badge } from "@/components/ui/badge";
 import Salute from "../components/salute";
 
 async function getHello() {
@@ -31,21 +29,30 @@ async function getHello() {
 export default async function ProfilesList() {
     const greeting = await getHello();
     return (
-        <section className="px-6 py-12 flex flex-col flex-wrap space-y-8">
+        <section className="my-12 mx-6 flex flex-col gap-12">
             <div className="flex justify-between items-end">
                 <CompanyLogo />
                 <Salute>{greeting?.hello || "Usuário"}</Salute>
             </div>
 
-            <div className="flex space-x-10">
-                <h1 className="text-2xl font-semibold">Lista de Perfis</h1>
-                <Button className="bg-indigo-950 font-bold">
-                    <Link href="/new-profile">+ NOVO PERFIL</Link>
+            <div className="flex justify-between items-center">
+                <h1 className="text-3xl font-semibold">Lista de Perfis</h1>
+                <Button className="bg-indigo-950 font-bold gap-2">
+                    <Plus />
+
+                    <Link className="text-xs" href="/new-profile">
+                        NOVO PERFIL
+                    </Link>
                 </Button>
             </div>
 
-            <div className="space-y-6">
-                <Label htmlFor="procurar e-mail">Procurar perfil</Label>
+            <div className="flex flex-col gap-2">
+                <Label
+                    className="text-sm font-semibold"
+                    htmlFor="procurar e-mail"
+                >
+                    Procurar perfil
+                </Label>
                 <div className="flex">
                     <Input
                         className="rounded-r-none"
@@ -55,55 +62,64 @@ export default async function ProfilesList() {
                         <Search />
                     </Button>
                 </div>
-                <div className="space-y-6">
-                    <Card>
-                        <CardHeader className="flex">
-                            <Avatar className="flex w-20 h-20">
-                                <AvatarImage src="" />
-                                <AvatarFallback className="bg-indigo-200 text-3xl">
+            </div>
+
+            <div className="px-2">
+                <Card className="border-slate-400">
+                    <CardHeader className="flex flex-row gap-4">
+                        <Avatar className="flex w-24 h-24">
+                            <AvatarImage src="" />
+                            <AvatarFallback className="bg-gray-100">
+                                <div className="text-slate-950 text-3xl">
                                     MS
-                                </AvatarFallback>
-                            </Avatar>
-                            <h1 className="font-bold text-2xl">
+                                </div>
+                            </AvatarFallback>
+                        </Avatar>
+
+                        <div className="flex flex-col justify-center gap-1">
+                            <h1 className="font-semibold text-2xl">
                                 Maria da Silva
                             </h1>
-                            <h2 className="font-light text-sm">
+                            <h2 className="font-medium text-xs text-slate-600">
                                 mariadasilva@marquei.com
                             </h2>
-                        </CardHeader>
-                        <CardContent className="space-y-2 font-medium">
-                            <Badge className="bg-green-200 text-black w-20 justify-center">
+                        </div>
+                    </CardHeader>
+
+                    <CardContent className="flex flex-col gap-2">
+                        <div>
+                            <Badge className=" bg-green-200 text-green-950 text-base font-medium justify-center px-8 py-1">
                                 ADMIN
                             </Badge>
-                            <h1 className="text-sm w-20 flex items-center justify-center">
-                                Gerente
-                            </h1>
-                        </CardContent>
+                        </div>
+                        <h1 className="text-base text-slate-600 font-medium">
+                            Gerente
+                        </h1>
+                    </CardContent>
 
-                        <CardFooter>
-                            <Accordion type="single" collapsible>
-                                <AccordionItem value="item-1">
-                                    <AccordionTrigger className="text-blue-500">
-                                        Ações disponíveis
-                                    </AccordionTrigger>
-                                    <AccordionContent className="flex flex-col text-indigo-950 text-sm space-y-2">
-                                        <Link href="">Editar perfil</Link>
-                                        <Link href="">Pedir nova senha</Link>
+                    <CardFooter>
+                        <Accordion type="single" collapsible className="w-full">
+                            <AccordionItem value="item-1">
+                                <AccordionTrigger className="text-blue-500">
+                                    Ações disponíveis
+                                </AccordionTrigger>
+                                <AccordionContent className="flex flex-col gap-5 text-slate-600 text-base mt-3">
+                                    <Link href="">Editar perfil</Link>
+                                    <Link href="">Pedir nova senha</Link>
+                                    <div className="flex justify-between">
                                         <Link href="">Desativar perfil</Link>
-                                        <div className="flex">
-                                            <Link href="">Excluir perfil</Link>
-                                            <h1 className="ml-32 text-sm text-green-600">
-                                                ATIVO
-                                            </h1>
-                                        </div>
-                                    </AccordionContent>
-                                </AccordionItem>
-                            </Accordion>
-                        </CardFooter>
-                    </Card>
-                </div>
+                                        <h1 className="text-base text-slate-400 tracking-widest">
+                                            ATIVO
+                                        </h1>
+                                    </div>
+
+                                    <Link href="">Excluir perfil</Link>
+                                </AccordionContent>
+                            </AccordionItem>
+                        </Accordion>
+                    </CardFooter>
+                </Card>
             </div>
         </section>
     );
 }
-

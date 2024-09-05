f1cfebd4f1805cc2762b20113fa435300523e7d5
Rafael <rafael2606arantes@gmail.com>
2024-09-05 14:42:29 -0300

    Search profile

--- api/src/http/routes/profiles-list.ts
+++ api/src/http/routes/profiles-list.ts
@@ -3,6 +3,7 @@ import { FastifyInstance } from "fastify";
 import { verify } from "jsonwebtoken";
 import { getJwtSecret } from "./utils/get-jwt-secret";
 import { getToken } from "./utils/get-token";
+import profileRepository from "../../repositories/profiles";
 
 export async function ProfilesList(server: FastifyInstance) {
     server.post("/profiles-list", async (request, reply) => {
@@ -15,11 +16,25 @@ export async function ProfilesList(server: FastifyInstance) {
 
         const userProfile = verify(token, secretKey) as {
             level: Level;
+            email: string;
         };
         if (userProfile.level === Level.USER) {
             return reply.status(401).send({
                 message: "Você não tem permissão para acessar esta tela!",
             });
+        }
+        if (userProfile.level === Level.ADMIN){
+            const profile = await profileRepository.findByEmail(
+                userProfile.email,
+            );
+
+            if (!profile) {
+                return reply
+                    .status(404)
+                    .send({ message: "Perfil não encontrado!" });
+            }
+
+            return reply.status(200).send({ profile });
         }
     });
 }

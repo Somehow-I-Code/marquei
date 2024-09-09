87e4559fd45bbd9b472fb4cc1409501937c3d1e0
jaqAmaral <jaqfreitas.amaral@gmail.com>
2024-09-09 11:52:55 -0300

    Add token nas rotas de categories e resources

--- api/src/http/routes/create-resources.ts
+++ api/src/http/routes/create-resources.ts
@@ -2,17 +2,37 @@ import { FastifyInstance } from "fastify";
 
 import resourcesRepository from "../../repositories/resources";
 import { createResource } from "../../validators/resources";
+import { getToken } from "./utils/get-token";
+import { verify } from "jsonwebtoken";
+import { getJwtSecret } from "./utils/get-jwt-secret";
 
 export async function createResources(server: FastifyInstance) {
     server.post("/resources", async (request, reply) => {
-        const { name, description, categoryId } = createResource.parse(
-            request.body,
-        );
+        const token = getToken(request.headers);
+        const secretKey = getJwtSecret();
+
+        if (!token) {
+            return reply.status(400).send({ message: "Token inválido!" });
+        }
+
+        const { name, description, categoryId, companyId } =
+            createResource.parse(request.body);
+
+        const profileSameCompany = verify(token, secretKey) as {
+            companyId: number;
+        };
+
+        if (profileSameCompany.companyId !== companyId) {
+            return reply.status(401).send({
+                message: "Você não tem permissão para executar esta operação!",
+            });
+        }
 
         const resource = await resourcesRepository.create({
             name,
             description,
             categoryId,
+            companyId,
         });
 
         return reply.status(201).send(resource);

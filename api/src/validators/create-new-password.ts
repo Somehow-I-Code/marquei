46cb85af340f6d1e5ff0379fc21c905e24579c1e
Rafael <rafael2606arantes@gmail.com>
2024-11-08 11:30:51 -0300

    Making suggested changes

--- api/src/validators/create-new-password.ts
+++ api/src/validators/create-new-password.ts
@@ -1,7 +1,10 @@
 import z from "zod";
 
 export const createNewPasswordSchema = z.object({
-    token: z.string().min(1, { message: "O token é obrigatório." }),
+    token: z.string({
+        required_error: "O token é obrigatório.",
+        invalid_type_error: "Formato do token inválido.",
+    }),
     newPassword: z.string().min(8, {
         message: "A senha nova deve conter no mínimo 8 caracteres.",
     }),

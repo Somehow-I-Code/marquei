01418d1492b377fe882e096d17d020d31acb1124
Rafael <rafael2606arantes@gmail.com>
2024-11-21 12:43:54 -0300

    Revert "Adding first midleware"
    This reverts commit 8d6d5b3535a604bab2693d6f1c32cdcadea0b340.

--- api/src/http/routes/change-password.ts
+++ api/src/http/routes/change-password.ts
@@ -4,117 +4,103 @@ import jwt from "jsonwebtoken";
 import { ZodError } from "zod";
 import profileRepository from "../../repositories/profiles";
 import { changePasswordSchema } from "../../validators/change-password";
-import { getToken } from "../routes/utils/get-token";
 import { ChangePasswordInput } from "./../../validators/change-password";
+import { getToken } from "../routes/utils/get-token";
 import { getJwtSecret } from "./utils/get-jwt-secret";
 import httpCodes from "./utils/http-codes";
 
 export async function changePassword(server: FastifyInstance) {
-    const midleware1 = (request, reply, next) => {
-        console.log("midleware1");
-        next();
-    };
+    server.patch("/change-password", async (request, reply) => {
+        const token = getToken(request.headers);
+        const secretKey = getJwtSecret();
 
-    server.patch(
-        "/change-password",
-        { preHandler: [midleware1] },
-        async (request, reply) => {
-            const token = getToken(request.headers);
-            const secretKey = getJwtSecret();
+        if (!token) {
+            return reply.send({ message: "Token inválido!" });
+        }
 
-            if (!token) {
-                return reply.send({ message: "Token inválido!" });
-            }
+        const decoded = jwt.verify(token, secretKey) as {
+            id: number;
+            firstLogin: boolean;
+        };
 
-            const decoded = jwt.verify(token, secretKey) as {
-                id: number;
-                firstLogin: boolean;
-            };
+        let changePassword: ChangePasswordInput;
 
-            let changePassword: ChangePasswordInput;
+        try {
+            changePassword = changePasswordSchema.parse(request.body);
+        } catch (error) {
+            return reply.status(httpCodes.BAD_REQUEST).send({
+                message: (error as ZodError).issues[0].message,
+            });
+        }
 
-            try {
-                changePassword = changePasswordSchema.parse(request.body);
-            } catch (error) {
-                return reply.status(httpCodes.BAD_REQUEST).send({
-                    message: (error as ZodError).issues[0].message,
-                });
-            }
+        const profile = await profileRepository.findById(decoded.id, true);
 
-            const profile = await profileRepository.findById(decoded.id, true);
+        if (!profile) {
+            return reply
+                .status(httpCodes.NOT_FOUND)
+                .send({ message: "Usuário não encontrado!" });
+        }
 
-            if (!profile) {
+        const { currentPassword, newPassword, repeatPassword } = changePassword;
+
+        if (decoded.firstLogin === false) {
+            if (!currentPassword) {
                 return reply
-                    .status(httpCodes.NOT_FOUND)
-                    .send({ message: "Usuário não encontrado!" });
+                    .status(httpCodes.BAD_REQUEST)
+                    .send({ message: "É obrigatório senha atual!" });
             }
 
-            const { currentPassword, newPassword, repeatPassword } =
-                changePassword;
-
-            if (decoded.firstLogin === false) {
-                if (!currentPassword) {
-                    return reply
-                        .status(httpCodes.BAD_REQUEST)
-                        .send({ message: "É obrigatório senha atual!" });
-                }
-
-                const passwordMatch = await bcrypt.compare(
-                    currentPassword,
-                    profile.password,
-                );
-
-                if (!passwordMatch) {
-                    return reply
-                        .status(httpCodes.UNAUTHORIZED)
-                        .send({ message: "Senha atual incorreta." });
-                }
-
-                if (currentPassword === newPassword) {
-                    return reply.status(httpCodes.BAD_REQUEST).send({
-                        message:
-                            "A nova senha não pode ser igual à senha antiga.",
-                    });
-                }
-            }
-
-            const newPasswordMatch = await bcrypt.compare(
-                newPassword,
+            const passwordMatch = await bcrypt.compare(
+                currentPassword,
                 profile.password,
             );
 
-            if (newPasswordMatch) {
+            if (!passwordMatch) {
+                return reply
+                    .status(httpCodes.UNAUTHORIZED)
+                    .send({ message: "Senha atual incorreta." });
+            }
+
+            if (currentPassword === newPassword) {
                 return reply.status(httpCodes.BAD_REQUEST).send({
                     message: "A nova senha não pode ser igual à senha antiga.",
                 });
             }
+        }
 
-            if (newPassword !== repeatPassword) {
-                return reply.status(httpCodes.BAD_REQUEST).send({
-                    message:
-                        "A sua confirmação de senha não bate com a nova senha.",
-                });
-            }
+        const newPasswordMatch = await bcrypt.compare(
+            newPassword,
+            profile.password,
+        );
 
-            const updatedProfile = await profileRepository.updatePassword(
-                profile.id,
-                newPassword,
-            );
+        if (newPasswordMatch) {
+            return reply.status(httpCodes.BAD_REQUEST).send({
+                message: "A nova senha não pode ser igual à senha antiga.",
+            });
+        }
 
-            const refreshedToken = jwt.sign(
-                {
-                    id: updatedProfile.id,
-                    email: updatedProfile.email,
-                    level: updatedProfile.level,
-                    companyId: updatedProfile.companyId,
-                    firstLogin: updatedProfile.firstLogin,
-                },
-                secretKey,
-            );
+        if (newPassword !== repeatPassword) {
+            return reply.status(httpCodes.BAD_REQUEST).send({
+                message: "A sua confirmação de senha não bate com a nova senha.",
+            });
+        }
 
-            return reply
-                .status(httpCodes.SUCCESS)
-                .send({ token: refreshedToken });
-        },
-    );
+        const updatedProfile = await profileRepository.updatePassword(
+            profile.id,
+            newPassword,
+        );
+
+        const refreshedToken = jwt.sign(
+            {
+                id: updatedProfile.id,
+                email: updatedProfile.email,
+                level: updatedProfile.level,
+                companyId: updatedProfile.companyId,
+                firstLogin: updatedProfile.firstLogin,
+            },
+            secretKey,
+        );
+
+        return reply.status(httpCodes.SUCCESS).send({ token: refreshedToken });
+    });
 }

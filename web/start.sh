800c803d78c9b8100a1dc2202b181d3ecc8cabd6
Thiago Gois <goislimat2@gmail.com>
2024-11-25 13:59:46 -0300

    Adiciona comentários em alguns arquivos the script pra facilitar o seu entendimento

--- web/start.sh
+++ web/start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# log a text so you know the script is running
 echo "installing dependencies and starting the server"
-npm ci && npm cache clean --force
+
+# install dependencies
+npm ci && npm cache clean
+
+# start the server and watch for changes in package.json and package-lock.json
+# if those files are changed, reinstall the dependencies
 exec npx nodemon --watch package.json --watch package-lock.json --exec "if [ package.json -nt node_modules ] || [ package-lock.json -nt node_modules ]; then npm ci && npm cache clean --force; fi; npm run dev"

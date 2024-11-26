6a62ec601675b6a230a04c3b178cfda252b7caed
Thiago Gois <goislimat2@gmail.com>
2024-11-25 22:15:21 -0300

    Torna os scripts de inicialização mais simples a fim de serem mais compatíveis com ambientes de desenvolvimento

--- api/start.sh
+++ api/start.sh
@@ -4,11 +4,11 @@
 echo "installing dependencies and starting the server"
 
 # install dependencies
-npm ci && npm cache clean
+npm install
 
 # generate prisma client javascript files
 npx prisma generate
 
 # start the server and watch for changes in package.json and package-lock.json
 # if those files are changed, reinstall the dependencies
-exec npx nodemon --watch package.json --watch package-lock.json --exec "if [ package.json -nt node_modules ] || [ package-lock.json -nt node_modules ]; then npm ci && npm cache clean --force; fi; npm run dev"
+exec npx nodemon --watch package.json --watch package-lock.json --exec "if [ package.json -nt node_modules ] || [ package-lock.json -nt node_modules ]; then npm install; fi; npm run dev"

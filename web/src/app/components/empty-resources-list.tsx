2023fb5c15389b52d3b58cde84601c4579b44431
Rafael <rafael2606arantes@gmail.com>
2024-11-28 15:38:26 -0300

    Create empty resources list component

---
+++ web/src/app/components/empty-resources-list.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Scheduling from "../home/assets/scheduling.svg";
+import CompanyLogo from "./company-logo";
+import Salute from "./salute";
+
+export default function EmptyResourcesList() {
+    return (
+        <>
+            <header className="flex justify-between items-end px-6 py-12">
+                <CompanyLogo />
+                <Salute>{"Usuário"}</Salute>
+            </header>
+
+            <section>
+                <div className="w-full max-w-sm mx-auto text-wrap bg-slate-100 p-4 rounded-lg">
+                    <p className="text-gray-950 text-base font-semibold p-2">
+                        Ahh não! Você ainda não tem nenhum recurso para agendar.
+                    </p>
+                </div>
+
+                <div className="flex justify-center items-center p-6">
+                    <Scheduling />
+                </div>
+
+                <div className="flex justify-center items-center p-2">
+                    <Button className="px-8 py-6 text-sm font-bold">
+                        {/* TODO: Vai para tela de agendamento */}
+                        <Link href="">CRIAR PRIMEIRO AGENDAMENTO</Link>
+                    </Button>
+                </div>
+            </section>
+        </>
+    );
+}
+
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,13 +1,11 @@
-import { Button } from "@/components/ui/button";
 import { ResourceResponse, Resources } from "@/types/resources";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 import BottomFloatingMenu from "./components/bottom-floating-menu";
 import CategoriesList from "./components/categories-list";
 import CompanyLogo from "./components/company-logo";
+import EmptyResourcesList from "./components/empty-resources-list";
 import ResourcesList from "./components/resources-list";
 import Salute from "./components/salute";
-import Scheduling from "./home/assets/scheduling.svg";
 import getSession from "./utis/get-session";
 
 function transformResources(resources: ResourceResponse) {
@@ -54,34 +52,7 @@ export default async function Home() {
     const resources = await getResources(session);
 
     if (resources.length === 0) {
-        return (
-            <>
-                <header className="flex justify-between items-end px-6 py-12">
-                    <CompanyLogo />
-                    <Salute>{"Usuário"}</Salute>
-                </header>
-
-                <section>
-                    <div className="w-full max-w-sm mx-auto text-wrap bg-slate-100 p-4 rounded-lg">
-                        <p className="text-gray-950 text-base font-semibold p-2">
-                            Ahh não! Você ainda não tem nenhum recurso para
-                            agendar.
-                        </p>
-                    </div>
-
-                    <div className="flex justify-center items-center p-6">
-                        <Scheduling />
-                    </div>
-
-                    <div className="flex justify-center items-center p-2">
-                        <Button className="px-8 py-6 text-sm font-bold">
-                            {/* TODO: Vai para tela de agendamento */}
-                            <Link href="">CRIAR PRIMEIRO AGENDAMENTO</Link>
-                        </Button>
-                    </div>
-                </section>
-            </>
-        );
+        return <EmptyResourcesList />;
     }
 
     const resourcesByCategory = getResourcesByCategory(resources);

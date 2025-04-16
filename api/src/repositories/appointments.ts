b956f488a5506b591ffe5bcb390b36bb11ea51f3
JaqAmaral <jaqfreitas.amaral@gmail.com>
2025-04-16 20:25:32 -0300

    modificado comentário

--- api/src/repositories/appointments.ts
+++ api/src/repositories/appointments.ts
@@ -9,26 +9,20 @@ class AppointmentsRepository {
 
     // Encontra um appointment através de uma data especifica (day)
     async findByDay(startsAt: Date | string) {
-        // Busca o início do dia.
-        const startOfDay = new Date(startsAt);
-        // Usa getDate para voltar um dia (Exemplo: 16 - 1 = 15, a data é 15)
-        startOfDay.setDate(startOfDay.getDate() - 1);
-        // Define o horário para o inicio do dia (Começa à meia-noite)
-        startOfDay.setHours(0, 0, 0, 0);
+        const startOfDay = new Date(startsAt); // Busca o início do dia.
+        startOfDay.setDate(startOfDay.getDate() - 1); // Volta um dia (Exemplo: 16 - 1 = 15, a data é 15)
+        startOfDay.setHours(0, 0, 0, 0); // Define o inicio do dia (Meia-noite)
 
-        // Busca o início do próximo dia.
-        const endOfDay = new Date(startsAt);
-        // Usa o getDate para avançar um dia (Exemplo: 16 + 1 = 17, a data é 17)
-        endOfDay.setDate(endOfDay.getDate() + 1);
-        // Define o horário para o início do próximo do dia (Antes de meia-noite).
-        endOfDay.setHours(0, 0, 0, 0);
+        const endOfDay = new Date(startsAt); // Busca o início do próximo dia.
+        endOfDay.setDate(endOfDay.getDate() + 1); //Avançar um dia (Exemplo: 16 + 1 = 17, a data é 17).
+        endOfDay.setHours(0, 0, 0, 0); // Define o início do próximo o dia (Antes de meia-noite).
 
         //Busca no banco de dados o dia desejado
         const appointments = await prisma.appointment.findMany({
             where: {
                 startsAt: {
-                    gte: startOfDay, // O gte(>=), pega o dia, por exemplo, >= 16/04/2025 00:00:00.
-                    lt: endOfDay, // O lt(<), pega antes do próximo dia, por exemplo, < 17/04/2025 00:00:00.
+                    gte: startOfDay, // O gte(>=), pega o dia (>= 16/04/2025 00:00:00).
+                    lt: endOfDay, // O lt(<), pega antes do próximo dia (< 17/04/2025 00:00:00).
                 },
             },
         });

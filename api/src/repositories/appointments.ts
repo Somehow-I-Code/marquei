60825da11589ebac7edbbd4728ea298d8b6530bb
JaqAmaral <jaqfreitas.amaral@gmail.com>
2025-04-17 12:35:23 -0300

    melhorando

--- api/src/repositories/appointments.ts
+++ api/src/repositories/appointments.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@lib/prisma";
+import { addDays, subDays } from "date-fns";
 
 class AppointmentsRepository {
     // Encontra um appointment através do seu id
@@ -8,21 +9,16 @@ class AppointmentsRepository {
     }
 
     // Encontra um appointment através de uma data especifica (day)
-    async findByDay(startsAt: Date | string) {
-        const startOfDay = new Date(startsAt); // Busca o início do dia.
-        startOfDay.setDate(startOfDay.getDate() - 1); // Volta um dia (Exemplo: 16 - 1 = 15, a data é 15)
-        startOfDay.setHours(0, 0, 0, 0); // Define o inicio do dia (Meia-noite)
-
-        const endOfDay = new Date(startsAt); // Busca o início do próximo dia.
-        endOfDay.setDate(endOfDay.getDate() + 1); //Avançar um dia (Exemplo: 16 + 1 = 17, a data é 17).
-        endOfDay.setHours(0, 0, 0, 0); // Define o início do próximo o dia (Antes de meia-noite).
+    async findByDay(startsAt: Date) {
+        const startOfDay = new Date(startsAt); // Pega o início da data informada
+        const endOfDay = addDays(startOfDay, 1); // Obtem o final do dia (Amanhã)
 
         //Busca no banco de dados o dia desejado
         const appointments = await prisma.appointment.findMany({
             where: {
                 startsAt: {
-                    gte: startOfDay, // O gte(>=), pega o dia (>= 16/04/2025 00:00:00).
-                    lt: endOfDay, // O lt(<), pega antes do próximo dia (< 17/04/2025 00:00:00).
+                    gte: startOfDay, // O gte(>=), início do dia (>= 2025-04-16).
+                    lt: endOfDay, // O lt(<), início do dia seguinte (< 2025/04/17).
                 },
             },
         });
